c003d4ce8ba72a89041856fceea545e73db38090
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-03-01 22:16:35 +0530

    Add project files.

---
+++ EmployeePayrollMultiThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollMultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
